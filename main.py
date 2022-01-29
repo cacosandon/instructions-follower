b8ae2da70d4b6ab4a07ad57bd9f87c0cc9609422
cacosandon <jiossandon@uc.cl>
2022-01-29 14:56:07 -0300

    chore(): run on production

--- main.py
+++ main.py
@@ -156,5 +156,5 @@ def invalid_route(e):
   return "Invalid route."
 
 if __name__ == "__main__":
-  app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 4444)), debug=True)
+  app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 4444)))
 
